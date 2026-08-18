d744bdfeb03d3202faeab799bde723b06ea23414
suyoglalge <suyoglalge21@gmail.com>
2026-08-19 00:45:36 +0530

    Add files via upload

---
+++ C_Programming/struct_demo4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#pragma pack (1) 
+struct demo
+{
+    int i;
+    char ch;  
+    float f;
+   
+};
+
+int main()
+{
+
+ struct demo dobj1;
+ struct demo dobj2;
+  
+ struct demo *dp=NULL;
+
+ dp=&dobj2;
+
+
+ //direct accessing operator .
+ dobj1.i=11;
+ dobj1.ch='A';  
+ dobj1.f=90.99f;
+
+//indirect accessing operator ->
+ dp-> i =21;
+ dp ->ch ='B';
+ dp->f=91.99;
+
+
+
+ printf("%d\n",dobj1.i);
+ printf("%d\n",dp->i);
+    return 0;
+} 
