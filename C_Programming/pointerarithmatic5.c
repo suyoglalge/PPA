370eb5ba2822133a6fadd8f6e83195d3a94c3184
suyoglalge <suyoglalge21@gmail.com>
2026-08-18 11:49:32 +0530

    Add files via upload

---
+++ C_Programming/pointerarithmatic5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+
+    int arr[]={11,21,51,101,111};
+    int *p= NULL;
+    int *q= NULL;
+
+    p = &(arr[1]);
+    q = &(arr[3]);
+     
+
+    printf("%d\n",*q);//101
+
+    q = q-3;
+
+    printf("%d\n",*q); //11
+
+   
+  
+ return 0;
+}
