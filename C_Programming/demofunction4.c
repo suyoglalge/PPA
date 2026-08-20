1d6900eecdcf6919048aa3a6e74824248dab5b8f
suyoglalge <suyoglalge21@gmail.com>
2026-08-20 21:26:06 +0530

    Add files via upload

---
+++ C_Programming/demofunction4.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+int main()
+{
+
+    int value1= 0,value2=0,ans = 0; 
+
+    printf("Enter first number:");
+    scanf("%d",&value1);
+
+    printf("Enter second number:");
+    scanf("%d",&value2);
+    
+    ans=value1+value2 ;//buisness logic
+
+    printf("Addition is :%d",ans);
+
+     
+
+    return 0;
+}
