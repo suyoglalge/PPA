9e1c60a479ae7d89b3e7b77ce74ad6b591546747
suyoglalge <suyoglalge21@gmail.com>
2026-08-20 21:25:41 +0530

    Add files via upload

---
+++ C_Programming/demofunction3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+ int value1 = 10;    
+ int value2=11;      
+ int ans = 0;       
+
+
+int main()
+{
+   
+ ans = value1 + value2;
+
+ printf("Addition is :%d\n",ans);
+
+    return 0;
+}
