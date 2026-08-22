9c1c7633496b620b3fc371ad344c593f94fc1a4e
suyoglalge <suyoglalge21@gmail.com>
2026-08-22 21:47:26 +0530

    Add files via upload

---
+++ C++_Programming/first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
+
+//cpp mdhe extension file deyaichi grj naste mhnun .h lihit nhi
+//g++ first.cpp -o myexe
+//myexe.exe
