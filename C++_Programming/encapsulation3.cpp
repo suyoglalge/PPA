06da34c1e543ca2100f6d2dfa74fb6a5048ef6dc
suyoglalge <suyoglalge21@gmail.com>
2026-08-23 21:14:38 +0530

    Add files via upload

---
+++ C++_Programming/encapsulation3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class demo //by default private access specifier
+{
+
+    int i; //data
+    char ch;
+    float f; //data 
+
+};
+
+int main()
+{
+
+    demo dobj;
+
+    dobj.i=11;
+    dobj.ch='A';
+    dobj.f=3.14;
+
+   
+
+    return 0;
+}
+
