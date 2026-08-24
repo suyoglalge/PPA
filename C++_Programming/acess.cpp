ca4277e151db7bde4c5d25d669a977243fd9b668
suyoglalge <suyoglalge21@gmail.com>
2026-08-24 22:22:20 +0530

    Add files via upload

---
+++ C++_Programming/acess.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class demo
+{
+  
+    int no;
+    float f;
+    double d;
+
+};
+int main()
+{
+   
+    cout<<sizeof(demo)<<"\n";
+
+    return 0;
+}
+
