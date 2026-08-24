fd5abdeaf2a5fcd811e1abc5aacf5232d063ec26
suyoglalge <suyoglalge21@gmail.com>
2026-08-24 22:23:04 +0530

    Add files via upload

---
+++ C++_Programming/acess3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class demo
+{
+  
+    public:
+    int no;
+    float f;
+    double d;
+
+};
+int main()
+{
+
+    demo dobj;
+    dobj.no=10;
+   
+    cout<<dobj.no<<"....\n";
+
+    return 0;
+}
