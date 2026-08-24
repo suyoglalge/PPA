4ecd676e309e46133a5841310b5962d9d94ea8ee
suyoglalge <suyoglalge21@gmail.com>
2026-08-24 22:22:42 +0530

    Add files via upload

---
+++ C++_Programming/acess2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#pragma pack(1)
+using namespace std;
+
+class demo
+{
+  
+    char ch;
+    int no;
+    float f;
+    double d;
+
+};
+int main()
+{
+   
+    cout<<sizeof(demo)<<"\n";
+
+    return 0;
+}
