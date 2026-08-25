d72e0c5b3e5da12b30c8f5cbff6741ef03857012
suyoglalge <suyoglalge21@gmail.com>
2026-08-25 23:13:30 +0530

    Add files via upload

---
+++ C++_Programming/substractioninput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int no1,no2,sub;
+
+    cout<<"enter first no:";
+    cin>>no1;
+
+    cout<<"Enter second number:";
+    cin>>no2;
+
+    sub=no1-no2;
+
+    cout<<"the substraction is :"<<sub;
+
+
+
+    return 0;
+}
