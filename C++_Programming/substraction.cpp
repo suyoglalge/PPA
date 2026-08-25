ce56860ea7a64b3f6ad4bc596c80f57ea72193a2
suyoglalge <suyoglalge21@gmail.com>
2026-08-25 23:12:54 +0530

    Add files via upload

---
+++ C++_Programming/substraction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a,b,sub;
+
+    a=20;
+    b=10;
+
+    sub=a-b;
+
+    cout<<"substraction is :"<<sub<<"\n";
+
+
+    return 0;
+}
