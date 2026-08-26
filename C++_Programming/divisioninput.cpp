affa7e4aa7469df5d103e2617596fd8221bcce07
suyoglalge <suyoglalge21@gmail.com>
2026-08-26 10:45:00 +0530

    Add files via upload

---
+++ C++_Programming/divisioninput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int no1,no2,div;
+
+    cout<<"enter first no:";
+    cin>>no1;
+
+    cout<<"Enter second number:";
+    cin>>no2;
+
+    div=no1 / no2;
+
+    cout<<"the substraction is :"<<div;
+
+
+
+    return 0;
+}
