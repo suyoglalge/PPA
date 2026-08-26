d876264be1d3140a58b79b939454a3ef79567504
suyoglalge <suyoglalge21@gmail.com>
2026-08-26 10:36:04 +0530

    Add files via upload

---
+++ C++_Programming/multiplicationinput.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int no1,no2,multiplication;
+
+
+    cout<<"enter first no:";
+    cin>>no1;
+
+    cout<<"enter second no:";
+    cin>>no2;
+
+    multiplication=no1*no2;
+
+    cout<<"multiplication is:"<<multiplication;
+
+
+
+
+    return 0;
+}
