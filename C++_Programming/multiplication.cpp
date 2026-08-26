98b53c75d50cbb847f4c25d3e203003a683d427e
suyoglalge <suyoglalge21@gmail.com>
2026-08-26 10:35:29 +0530

    Add files via upload

---
+++ C++_Programming/multiplication.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int no1,no2,multiplication;
+
+    no1=12;
+    no2=10;
+    multiplication=no1*no2;
+
+    cout<<"multiplication is :"<<multiplication;
+
+
+    return 0;
+}
