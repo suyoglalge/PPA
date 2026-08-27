27b3aff7e585f5541defc13fb686a47103d2ba58
suyoglalge <suyoglalge21@gmail.com>
2026-08-27 15:34:13 +0530

    Add files via upload

---
+++ Java_Programming/Demo.java
@@ -0,0 +1,13 @@
+class Demo 
+{
+    public static void main(String A[] ) 
+    {
+    System.out.println("Jay Ganesh");
+    }
+}
+
+
+//java Demo.java
+//javac Demo.java---for mac
+//java Demo
+//system cha aat jaun out cha aat jaun print kra
