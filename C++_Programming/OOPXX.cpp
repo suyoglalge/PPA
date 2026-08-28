f05e69150a10e14dea754d982d80881b99f7f8cf
suyoglalge <suyoglalge21@gmail.com>
2026-08-28 22:03:35 +0530

    Add files via upload

---
+++ C++_Programming/OOPXX.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Arithmatic
+{
+    public:
+        int No1;
+        int No2;
+
+        Arithmatic()
+        {
+            this->No1 = 0;
+            this->No2 = 0;
+        }
+        
+        Arithmatic(int i, int j)
+        {
+            this->No1 = i;
+            this->No2 = j;
+        }
+
+        // int Addition(Arithmatic *this)
+        int Addition()
+        {
+            int Ans = 0;
+            Ans = this->No1 + this->No2;
+            return Ans;
+        }
+
+        // int Substraction(Arithmatic *this)
+        int Substraction()
+        {
+            int Ans = 0;
+            Ans = this->No1 - this->No2;
+            return Ans;
+        }
+};
+
+int main ()
+{
+    Arithmatic Aobj1(21,10);
+    int Result = 0;
+
+    // Result = Addition(&Aobj1);
+    Result = Aobj1.Addition();
+
+    cout<<"Addition is : "<<Result<<"\n";
+
+    // Result = Substraction(&Aobj1);
+    Result = Aobj1.Substraction();
+
+    cout<<"Substraction is : "<<Result<<"\n";
+    
+    return 0;
+}
