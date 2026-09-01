d764a291995f907c2bb175288ca084e5e1dafaab
suyoglalge <suyoglalge21@gmail.com>
2026-09-02 00:07:14 +0530

    Add files via upload

---
+++ practice codes/SingleLevelpX.cpp
@@ -0,0 +1,80 @@
+//size of parents
+#include <iostream>
+using namespace std;
+class Parents
+{
+    public:
+      int home;
+      int money;
+
+      Parents()
+      {
+        home = 500;
+        money = 0;
+        cout<<"inside parents constructor"<<"\n";
+      }
+
+      Parents(int i , int j)
+      {
+        home=i;
+        money=j;
+        cout<<"inside parents parermetrized constructor"<<"\n";
+      }
+
+      ~Parents()
+      {
+        cout<<"inside parents Destructor"<<"\n";
+      }
+
+      int Salary()
+      {
+        int add;
+        add = home + money;
+      }
+
+      int Expenses()
+      {
+        int sub;
+        sub = home - money;
+        return sub;
+     }
+
+};
+class child : public Parents
+{
+    public:
+      int bike;
+      int collage;
+
+      child()
+      {
+        bike = 1;
+        collage = 1;
+        cout<<"inside Child constructor"<<"\n";
+      }
+
+      child(int i , int j)
+      {
+        bike=i;
+        collage = j;
+        cout<<"inside Child parematerized constructor"<<"\n";
+      }
+
+      int Gym()
+      {
+        int gym;
+        gym = money + home;
+        return gym;
+      }
+
+
+};
+int main()
+{
+    Parents pobj;
+
+    cout<<sizeof(pobj)<<"\n";
+
+
+    return 0;
+}
