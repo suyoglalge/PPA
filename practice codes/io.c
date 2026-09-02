e0b36d913dc7e71543f70fcf1a31bae6dd4b5ade
suyoglalge <suyoglalge21@gmail.com>
2026-09-02 22:02:51 +0530

    Add files via upload

---
+++ practice codes/io.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int no1, no2,ans;
+
+    printf("Enyer first number :");
+    scanf("%d",&no1);
+
+    printf("Enter first number :");
+    scanf("%d",&no2);
+
+    ans = no1+no2;
+
+    printf("The addition is : %d",ans);
+    return 0;
+}
