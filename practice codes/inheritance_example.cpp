975dc185be19cb0eb41292d1cd011710873a67b8
suyoglalge <suyoglalge21@gmail.com>
2026-09-04 22:04:29 +0530

    Add files via upload

---
+++ practice codes/inheritance_example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Animal 
+{
+public:
+    void eat() 
+    {
+        cout << "Animal is eating" << endl;
+    }
+};
+
+class Dog : public Animal 
+{
+public:
+    void bark() 
+    {
+        cout << "Dog is barking" ;
+    }
+};
+
+int main() 
+{
+    Dog d;
+    d.eat();   
+    d.bark();  
+    return 0;
+}
