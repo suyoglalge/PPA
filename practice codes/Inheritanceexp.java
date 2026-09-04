f68f1c5160fad7f1675ca24bf0b93486c4f6f677
suyoglalge <suyoglalge21@gmail.com>
2026-09-04 22:05:12 +0530

    Add files via upload

---
+++ practice codes/Inheritanceexp.java
@@ -0,0 +1,19 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Inheritanceexp {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();   // inherited
+        d.bark();  // own method
+    }
+}
