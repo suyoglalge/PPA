ecc5807882c763b6e30855f42a4f1f84e25e1cd5
suyoglalge <suyoglalge21@gmail.com>
2026-09-08 00:11:17 +0530

    Add files via upload

---
+++ Java_Programming/Sequence1.java
@@ -0,0 +1,13 @@
+class Sequence1
+{
+    public static void main(String[] args)
+    {
+        
+
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+        
+    }
+}
