b6e26efc555011bb20d6e0f3dc0b04370a410513
suyoglalge <suyoglalge21@gmail.com>
2026-09-10 00:26:50 +0530

    Add files via upload

---
+++ Java_Programming/Iteration3.java
@@ -0,0 +1,15 @@
+class Iteration2
+{
+    public static void main(String[] args)
+    { int i = 0;//1
+
+              //2
+        while (i<=4) 
+        {
+            System.out.println(i + " Jay Ganesh");//4  //i takl mhnun indexing disl output mdhe
+            i++;//3
+        }
+    }
+}
+
+//sequence of writing loop is 1 ,4 , 2 ,3
