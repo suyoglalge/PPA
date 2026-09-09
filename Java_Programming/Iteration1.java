f1605cb8b20c9024e1161764bd1a288997f03839
suyoglalge <suyoglalge21@gmail.com>
2026-09-10 00:25:56 +0530

    Add files via upload

---
+++ Java_Programming/Iteration1.java
@@ -0,0 +1,12 @@
+class Iteration1
+{
+    public static void main(String[] args)
+    { int i = 0;
+        
+       //    1   2    3  
+        for(i=1;i<=4;i++)
+        {
+           System.out.println("Jay Ganesh");//4
+        }
+    }
+}
