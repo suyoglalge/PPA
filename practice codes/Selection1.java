ba0e98513949036ec53a9f2ce5df9d56f5cf16ed
suyoglalge <suyoglalge21@gmail.com>
2026-09-11 19:32:11 +0530

    Add files via upload

---
+++ practice codes/Selection1.java
@@ -0,0 +1,16 @@
+class Selection1
+{
+    public static void main(String[] args) 
+    {
+        int a = 10;
+        int b = 20;
+
+        if (a>=b)
+        {
+            System.out.println("10 is greater then 20");
+        }
+        else{
+            System.out.println("20 is Greater then 10");
+        }
+    }
+}
