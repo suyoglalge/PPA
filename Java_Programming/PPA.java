6cebb97ddcc5fc49b6f6482e46709cfb9487a74d
suyoglalge <suyoglalge21@gmail.com>
2026-09-12 22:32:25 +0530

    Add files via upload

---
+++ Java_Programming/PPA.java
@@ -0,0 +1,13 @@
+package Marvellous; // createdbmarvellous packages and saying hya package mdhe ppa taka
+
+public class PPA//public writeen for first time
+ {
+    public void PPA_fun()
+    {
+      System.out.println("INside fun of PPA");
+    }  
+ }
+
+ // hyo server aahe hyala kon tari  call krel tya mul hya madhe main funtion nhi
+
+ //javac -d . PPA.java for loading this file in Marvellous folder
