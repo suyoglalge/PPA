e4ee63825714868b5fade49f56f3d4730f4c2b03
suyoglalge <suyoglalge21@gmail.com>
2026-09-12 22:32:56 +0530

    Add files via upload

---
+++ Java_Programming/Python.java
@@ -0,0 +1,15 @@
+package Marvellous.infosystem; // createdbmarvellous packages and saying hya package mdhe python taka
+
+public class Python
+ {
+    public void PPA_fun()
+    {
+      System.out.println("INside fun ofpython");
+    }  
+ }
+
+ // hyo server aahe hyala kon tari  call krel tya mul hya madhe main funtion nhi
+
+ //javac -d . PPA.java for loading this file in Marvellous folder
+
+ // interview import questions which will import
