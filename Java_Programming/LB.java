2956e1042163a4cc0bfe552b2ff9e7655bc3bd19
suyoglalge <suyoglalge21@gmail.com>
2026-09-12 22:30:30 +0530

    Add files via upload

---
+++ Java_Programming/LB.java
@@ -0,0 +1,11 @@
+package Marvellous; // created packages and saying hya package mdhe LB taka
+
+public class LB
+ {
+    public void LB_fun()
+    {
+      System.out.println("INside fun of LB");
+    }  
+ }
+
+ // hyo server aahe hyala kon tari  call krel tya mul hya madhe main funtion nhi
