2f2b29b59d99df805c663ba756a6cc52d0b1aeb0
suyoglalge <suyoglalge21@gmail.com>
2026-09-13 23:02:53 +0530

    Add files via upload

---
+++ practice codes/Suyogfe.java
@@ -0,0 +1,12 @@
+package sinhgad.fe;
+
+public class Suyogfe
+{
+    public void suyogfe()
+    {   System.out.println("Name:Suyog Lalge");
+        System.out.println("Address:Ahilyanagar , Tal.Ahilyanagar , Zilla.Ahilyanagar");
+        System.out.println("DOB:21-Nov-2006");
+        System.out.println("YOE:2024");
+        System.out.println("7.92 CGPA");
+    }
+}
