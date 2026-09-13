76b9f5ad2956c2f189677763df50e16269aed419
suyoglalge <suyoglalge21@gmail.com>
2026-09-13 23:02:10 +0530

    Add files via upload

---
+++ practice codes/Namratafe.java
@@ -0,0 +1,12 @@
+package sinhgad.fe;
+
+public class Namratafe {
+    public void namratafe()
+    {
+        System.out.println("Name:Namrata pawar");
+        System.out.println("Address:Chas Kamargaon , Tal.Ahilyanagar , Zilla.Ahilyanagar");
+        System.out.println("DOB:10-Jan-2006");
+        System.out.println("YOE:2024");
+        System.out.println("78 CCGPA");
+    }
+}
