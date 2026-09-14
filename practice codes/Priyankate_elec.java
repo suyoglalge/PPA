d7a97740a0c29a640c01b00ed8d24c3c431c24c3
suyoglalge <suyoglalge21@gmail.com>
2026-09-14 22:57:19 +0530

    Add files via upload

---
+++ practice codes/Priyankate_elec.java
@@ -0,0 +1,13 @@
+package sinhgad.te;
+
+public class Priyankate_elec {
+    public void teelec()
+    {
+        System.out.println("Name:Priyanka Dandile");
+        System.out.println("Address:PAtan , Tal.Patan , Zilla.Satara");
+        System.out.println("DOB:1-July-2026");
+        System.out.println("YOE:2024");
+        System.out.println("TE elec CGPA : 8.98");
+    }
+    
+}
