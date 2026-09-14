bf04d710fe7fef3b41e06361ac2d35bdf1e688d1
suyoglalge <suyoglalge21@gmail.com>
2026-09-14 22:54:39 +0530

    Add files via upload

---
+++ practice codes/Suyogse_comp.java
@@ -0,0 +1,13 @@
+package sinhgad.se;
+
+public class Suyogse_comp {
+    public void secomp()
+    {
+        System.out.println("Name:Suyog Lalge");
+        System.out.println("Address:Ahilyanagar , Tal.Ahilyanagar , Zilla.Ahilyanagar");
+        System.out.println("DOB:21-Nov-2006");
+        System.out.println("YOE:2024");
+        System.out.println("SE comp CGPA: 8.20" );
+    } 
+    
+}
