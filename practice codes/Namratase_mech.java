c25c1488d628631f3586bfe2e35982a3260f72af
suyoglalge <suyoglalge21@gmail.com>
2026-09-14 22:53:45 +0530

    Add files via upload

---
+++ practice codes/Namratase_mech.java
@@ -0,0 +1,13 @@
+package sinhgad.se;
+
+public class Namratase_mech {
+    public void semech()
+    {
+            System.out.println("Name:Namrata pawar");
+        System.out.println("Address:Chas Kamargaon , Tal.Ahilyanagar , Zilla.Ahilyanagar");
+        System.out.println("DOB:10-Jan-2006");
+        System.out.println("YOE:2024");
+        System.out.println("SE mech CGPA: 8.30" );
+    } 
+    
+}
