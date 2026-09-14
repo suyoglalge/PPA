01b0a0f3c66c9ff164201e83209ac32808d0b738
suyoglalge <suyoglalge21@gmail.com>
2026-09-14 22:54:07 +0530

    Add files via upload

---
+++ practice codes/Priyankase_elec.java
@@ -0,0 +1,13 @@
+package sinhgad.se;
+
+public class Priyankase_elec {
+    public void seelec()
+    {
+        System.out.println("Name:Priyanka Dandile");
+        System.out.println("Address:PAtan , Tal.Patan , Zilla.Satara");
+        System.out.println("DOB:1-July-2026");
+        System.out.println("YOE:2024");
+        System.out.println("SE elec CGPA: 8.20" );
+    } 
+    
+}
