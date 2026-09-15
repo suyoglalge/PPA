63ce33c4af87571c840377f112b3b77eb61ddf26
suyoglalge <suyoglalge21@gmail.com>
2026-09-15 23:20:04 +0530

    Add files via upload

---
+++ practice codes/Packg.java
@@ -0,0 +1,19 @@
+  import Marvellous.PPA;
+import Marvellous.infosystem.Python;
+import Marvellous.LB;
+
+class Packg
+{
+    public static void main(String[] args) 
+    {
+        PPA pobj = new PPA();
+        Python pyobj = new Python();
+        LB lobj = new LB();
+
+        pobj.PPA_fun();
+        lobj.LB_fun();
+        pyobj.PPA_fun();
+    }
+}
+
+    
