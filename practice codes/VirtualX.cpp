0a557d52e8fa1b58874abb92a33c9da3fb400256
suyoglalge <suyoglalge21@gmail.com>
2026-09-21 23:04:58 +0530

    Add files via upload

---
+++ practice codes/VirtualX.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void sound()
+    {
+        cout << "Animal makes a sound" << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void sound()
+    {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main()
+{
+    Animal *a;
+
+    Dog d;
+
+    a = &d;
+
+    a->sound();
+
+    return 0;
+}
