1e76afd3d7cc0e9d3eab4ab645255b304ee23b10
suyoglalge <suyoglalge21@gmail.com>
2026-09-22 23:49:56 +0530

    Add files via upload

---
+++ C++_Programming/PrintArrayElements.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
