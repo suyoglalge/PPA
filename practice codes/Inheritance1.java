03415ea8615b9e6de255a26e50ede176c85c5a54
suyoglalge <suyoglalge21@gmail.com>
2026-09-23 21:13:42 +0530

    Add files via upload

---
+++ practice codes/Inheritance1.java
@@ -0,0 +1,30 @@
+// Parent class
+class Animal {
+    String name;
+
+    void eat() {
+        System.out.println(name + " is eating.");
+    }
+}
+
+// Child class inherits Animal
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println(name + " is barking.");
+    }
+}
+
+public class Inheritance1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+
+        dog.name = "Buddy";
+
+        // Inherited from Animal
+        dog.eat();
+
+        // Defined in Dog
+        dog.bark();
+    }
+}
