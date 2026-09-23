e6a58a719c8e04e959ecc94cb72da610ad72209f
suyoglalge <suyoglalge21@gmail.com>
2026-09-23 21:14:11 +0530

    Add files via upload

---
+++ practice codes/Inheritanceconst.java
@@ -0,0 +1,28 @@
+class Animal {
+    Animal() {
+        System.out.println("Animal constructor called");
+    }
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        System.out.println("Dog constructor called");
+    }
+
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Inheritanceconst {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.eat();
+        d.bark();
+    }
+}
